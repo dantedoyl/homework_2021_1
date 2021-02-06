59b168fc719b333aec4e9fdf771db91744bbd5e4
dantedoyl <makstorzhkov@yandex.ru>
2021-02-07 00:48:21 +0300

    fixes

--- source/plain.js
+++ source/plain.js
@@ -1,3 +1,20 @@
 'use strict';
-
-const plain = array => array.flat(Infinity);
+/***
+ * Function creates a new array with all sub-array elements concatenated into it
+ *
+ * @param {Array} array - intial array
+ * @returns {Array}
+ */
+const plain = (array) => {
+        const result = [];
+        while (array.length) {
+            const next = array.pop();
+            if (Array.isArray(next) ) {
+                array.push(...next);
+            } else {
+                if (next !== undefined)
+                    result.push(next);
+            }
+        }
+        return result.reverse();
+};
